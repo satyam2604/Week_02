4dd71d68a4b46e37aa939f8230743f9e424b8691
Satyam Agrawal <satyamagrawalv2604@gmail.com>
2025-01-18 15:59:31 +0530

    hotel booking

--- Level_01/UseHotelBooking.java
+++ Level_01/UseHotelBooking.java
@@ -10,22 +10,58 @@ package Week_02.Day_02.Level_01;
  * @author User
  */
 public class UseHotelBooking {
+
     public static void main(String[] args) {
-        
+        System.out.println("Default constructor called!!!");
+        // create object of HotelBooking class and call default constructor
+        HotelBooking hotel1 = new HotelBooking();
+        // call a method to display hotel details
+        hotel1.displayDetails();
+        System.out.println("Parameterized constructor called!!!");
+        // create object of HotelBooking class and call parameterized constructor
+        HotelBooking hotel2 = new HotelBooking("Shaun", "Double", 3);
+        // call a method to display hotel details
+        hotel2.displayDetails();
+        System.out.println("Copy constructor called!!!");
+        // create object of HotelBooking class and call copy constructor
+        HotelBooking hotel3 = new HotelBooking(hotel2);
+        // call a method to display hotel details
+        hotel3.displayDetails();
     }
 }
-class HotelBooking{
+
+class HotelBooking {
+
     // default variables should be initialized
     private String guestName;
     private String roomType;
     private int nights;
 
+    // create a default constructor
+    public HotelBooking() {
+        guestName = "Peter";
+        roomType = "Single";
+        nights = 2;
+    }
+
+    // create a parameterized cunstructor 
     public HotelBooking(String guestName, String roomType, int nights) {
         this.guestName = guestName;
         this.roomType = roomType;
         this.nights = nights;
     }
-    
-    
-    
+
+    // create a copy constructor
+    public HotelBooking(HotelBooking hotel) {
+        this.guestName = hotel.guestName;
+        this.nights = hotel.nights;
+        this.roomType = hotel.roomType;
+    }
+
+    // create a method to diplay details
+    public void displayDetails() {
+        System.out.println("Guest name: " + guestName);
+        System.out.println("Room type: " + roomType);
+        System.out.println("Nights: " + nights);
+    }
 }
