cc66ddcb14ff2aca96ab2411794badb1ffe21b44
Satyam Agrawal <satyamagrawalv2604@gmail.com>
2025-01-18 15:10:43 +0530

    Book

--- Level_01/UseBook.java
+++ Level_01/UseBook.java
@@ -3,13 +3,32 @@
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
-package Week_02.Day_02.Level_01.HandleBook;
+package Week_02.Day_02.Level_01;
 
 /**
  *
  * @author User
  */
-public class Book {
+public class UseBook {
+
+    public static void main(String[] args) {
+
+        System.out.println("Non parameterised constructor is called!!");
+        // Create a Book object using the default constructor
+        Book book = new Book();
+        // Display details of the book
+        book.displayBookDetails();
+
+        System.out.println();
+        System.out.println("Parameterised constructor is called!!");
+        // Create a Book object using the parameterized constructor
+        Book books = new Book("C++", "By me", 250.36);
+        // Display details of the book
+        books.displayBookDetails();
+    }
+}
+
+class Book {
 
     // declare attributes of book
     private String title;
