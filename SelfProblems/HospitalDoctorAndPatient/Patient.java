475360784f1d1696163c2a33a402b6f93e56b4ce
Satyam Agrawal <satyamagrawalv2604@gmail.com>
2025-01-22 10:50:51 +0530

    hospital management

---
+++ SelfProblems/HospitalDoctorAndPatient/Patient.java
@@ -0,0 +1,24 @@
+package Week_02.Day_04.SelfProblems.HospitalDoctorAndPatient;
+
+import java.util.ArrayList;
+import java.util.List;
+
+// Patient Class
+public class Patient {
+    private String name; // Patient name
+
+    // Constructor
+    public Patient(String name) {
+        this.name = name;
+    }
+
+    // Getter method to get the name of the patient
+    public String getName() {
+        return name;
+    }
+
+    // Method to display the name of the patient
+    public void display() {
+        System.out.println("Patient: " + name);
+    }
+}
