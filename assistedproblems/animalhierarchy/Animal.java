69edd7f421235cd940f25fe025827ce442287793
Satyam Agrawal <satyamagrawalv2604@gmail.com>
2025-01-24 11:04:58 +0530

    animal hierarchy

--- assistedproblems/animalhierarchy/Animal.java
+++ assistedproblems/animalhierarchy/Animal.java
@@ -3,7 +3,7 @@ package Week_02.Day_05.assistedproblems.animalhierarchy;
 public class Animal {
     // attributes common to all animals
     String name; // name of Animal
-    int age; // age of animal
+     int age; // age of animal
     // constructor to initialize name and age to animal
     public Animal(String name, int age) {
         this.name = name;
